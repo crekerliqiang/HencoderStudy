9f9fca4f414b7ff9558f9f74e38f635c3161dafe
liqiang <liqiang@ickkey.com>
2019-04-03 09:58:34 +0800

    add practice2

---
+++ PracticeDraw2/app/src/main/java/com/hencoder/hencoderpracticedraw2/practice/Practice02RadialGradientView.java
@@ -0,0 +1,41 @@
+package com.hencoder.hencoderpracticedraw2.practice;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RadialGradient;
+import android.graphics.Shader;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+
+public class Practice02RadialGradientView extends View {
+
+    Paint mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+
+    public Practice02RadialGradientView(Context context) {
+        super(context);
+    }
+
+    public Practice02RadialGradientView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice02RadialGradientView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+
+        Shader shader = new RadialGradient(300, 300, 200, Color.parseColor("#E91E63"),
+                Color.parseColor("#2196F3"), Shader.TileMode.CLAMP);
+        mPaint.setShader(shader);
+        
+    }
+
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        canvas.drawCircle(300, 300, 200, mPaint);
+    }
+}
