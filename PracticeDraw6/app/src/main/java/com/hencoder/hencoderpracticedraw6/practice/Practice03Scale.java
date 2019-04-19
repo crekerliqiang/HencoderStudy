56869f515308423eee3c3ab89dfa8202a754a80e
liqiang <liqiang@ickkey.com>
2019-04-19 22:53:20 +0800

    add practice6

---
+++ PracticeDraw6/app/src/main/java/com/hencoder/hencoderpracticedraw6/practice/Practice03Scale.java
@@ -0,0 +1,58 @@
+package com.hencoder.hencoderpracticedraw6.practice;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import com.hencoder.hencoderpracticedraw6.R;
+
+public class Practice03Scale extends RelativeLayout {
+    Button animateBt;
+    ImageView imageView;
+    int scale = 0;
+    public Practice03Scale(Context context) {
+        super(context);
+    }
+
+    public Practice03Scale(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public Practice03Scale(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    protected void onAttachedToWindow() {
+        super.onAttachedToWindow();
+
+        animateBt = (Button) findViewById(R.id.animateBt);
+        imageView = (ImageView) findViewById(R.id.imageView);
+
+        animateBt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                // TODO 在这里处理点击事件，通过 View.animate().scaleX/Y() 来让 View 放缩
+                switch (scale){
+                    case 0:
+                        imageView.animate().scaleX(0.5f);
+                        imageView.animate().scaleY(0.5f);
+                    break;
+                    case 1:
+                        imageView.animate().scaleX(2f);
+                        imageView.animate().scaleY(2f);
+                        break;
+
+                }
+                scale++;
+                if(scale > 1){
+                    scale = 0;
+                }
+            }
+        });
+    }
+}
