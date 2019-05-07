9b13701f39b32568ae750372cb0a9d9fc3a7abfb
liqiang-t <liqiang-t@oppo.com>
2019-05-07 18:07:15 +0800

    add 双指缩放

--- Plus/16scalable/src/main/java/demo/com/scalable/view/ScalableImageView.java
+++ Plus/16scalable/src/main/java/demo/com/scalable/view/ScalableImageView.java
@@ -9,20 +9,17 @@ import android.support.v4.view.GestureDetectorCompat;
 import android.util.AttributeSet;
 import android.view.GestureDetector;
 import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.widget.OverScroller;
+import android.widget.Toast;
 
+import demo.com.library.LLog;
 import demo.com.library.Util;
 
-
-//方法关键方法：动画
-//移动关键方法：translation
-//惯性滑动关键方法：.OverScroller.fling();
-public class ScalableImageView extends View implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener, Runnable {
-
-    private static final String TAG = ScalableImageView.class.getSimpleName();
+//TODO 加入双指缩放以后，变得不是很流畅，需要改进地方
+public class ScalableImageView extends View{
     private static final float OVER_SCALE_RATE = 3f;
-
     private Bitmap bitmap;
     private Paint paint;
     private float left;
@@ -32,25 +29,37 @@ public class ScalableImageView extends View implements GestureDetector.OnGesture
     boolean isBig;
     private float smallScale;
     private float bigScale;
-    private float scaleRate;//放大率
-    GestureDetectorCompat detector;
-    ObjectAnimator animation;
-    OverScroller scroller;
+    private float scaleMultiple;//
+    //点击检测
+    private GestureDetectorCompat detector;
+    //属性动画
+    private ObjectAnimator animation;
+    //惯性滑动
+    private OverScroller scroller;
+    //点击检测和惯性滑动的监听
+    private MyGestureListener myGestureListener;
+    //
+    private MyRunnable runnable;
+    //双指缩放
+    ScaleGestureDetector scaleGestureDetector;
     public ScalableImageView(Context context, AttributeSet attrs) {
         super(context, attrs);
         paint = new Paint(Paint.ANTI_ALIAS_FLAG);
         bitmap = Util.getBitmap();
-        detector = new GestureDetectorCompat(context,this);
-        detector.setOnDoubleTapListener(this);
-        animation = ObjectAnimator.ofFloat(this,"scaleRate",0f,1f);
+        myGestureListener = new MyGestureListener();
+        runnable = new MyRunnable();
+        detector = new GestureDetectorCompat(context, myGestureListener);
+        detector.setOnDoubleTapListener(myGestureListener);
+        animation = ObjectAnimator.ofFloat(this,"scaleMultiple",1);
         scroller = new OverScroller(context);
+        scaleGestureDetector = new ScaleGestureDetector(context,new ScaleListener());
     }
 
-    public float getScaleRate() {
-        return scaleRate;
+    public float getScaleMultiple() {
+        return scaleMultiple;
     }
-    public void setScaleRate(float scaleRate) {
-        this.scaleRate = scaleRate;
+    public void setScaleMultiple(float scaleMultiple) {
+        this.scaleMultiple = scaleMultiple;
         invalidate();
     }
     //说明：使用该方法，可以在获取View的宽度，相当于在onDraw里面使用getWidth
@@ -67,95 +76,145 @@ public class ScalableImageView extends View implements GestureDetector.OnGesture
             smallScale = (float) getHeight() / (float)bitmap.getHeight();
             bigScale = (float) getWidth() / (float)bitmap.getWidth();
         }
+        scaleMultiple = smallScale;
+        animation.setFloatValues(smallScale,bigScale);
         bigScale *= OVER_SCALE_RATE;
     }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return detector.onTouchEvent(event);
+        //发生冲突时，优先使用 scaleGestureDetector
+        boolean result = scaleGestureDetector.onTouchEvent(event);
+        if(!scaleGestureDetector.isInProgress()){
+            result = detector.onTouchEvent(event);
+        }
+        return result;
     }
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         reviseXY();
-        canvas.translate(offsetX * scaleRate,offsetY * scaleRate);//scaleRate 保证缩小以后回到原位
-        float scale = smallScale + (bigScale - smallScale) * scaleRate;
-        canvas.scale(scale,scale,getWidth()/2f,getHeight()/2f);
+        float rate =  (scaleMultiple*smallScale)/bigScale;
+        canvas.translate(offsetX * rate,offsetY * rate);//scaleRate 保证缩小以后回到原位
+        canvas.scale(scaleMultiple,scaleMultiple,getWidth()/2f,getHeight()/2f);
         canvas.drawBitmap(bitmap,left,top,paint);
     }
 
+    //点击检测以及惯性滑动
+    private class MyGestureListener implements GestureDetector.OnGestureListener, GestureDetector.OnDoubleTapListener{
+        //DOWN
+        @Override
+        public boolean onDown(MotionEvent e) {
+            return true;
+        }
 
-    //DOWN
-    @Override
-    public boolean onDown(MotionEvent e) {
-        return true;
+        @Override
+        public void onShowPress(MotionEvent e) {
+
+        }
+
+        @Override
+        public boolean onSingleTapUp(MotionEvent e) {
+            return false;
+        }
+
+        //滑动
+        @Override
+        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+            if(isBig){
+                offsetX = offsetX - distanceX;
+                offsetY = offsetY - distanceY;
+                invalidate();
+            }
+            return false;
+        }
+
+        @Override
+        public void onLongPress(MotionEvent e) {
+
+        }
+
+        //惯性滑动
+        @Override
+        public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+            scroller.fling((int)offsetX,(int)offsetY,(int)velocityX,(int)velocityY,
+                    -(int)(bitmap.getWidth()*bigScale - getWidth()),
+                    (int)(bitmap.getWidth()*bigScale - getWidth()),
+                    -(int)(bitmap.getHeight()*bigScale - getHeight()),
+                    (int)(bitmap.getHeight()*bigScale - getHeight()));
+            postOnAnimation(runnable);
+            return false;
+        }
+
+        //确认单击[第一次按下后，一段时间内(1500ms貌似)没有第二次点击]
+        @Override
+        public boolean onSingleTapConfirmed(MotionEvent e) {
+            return false;
+        }
+
+        //双击
+        @Override
+        public boolean onDoubleTap(MotionEvent e) {
+            isBig = !isBig;
+            if(isBig){
+                //点击放大时，点到哪里就从哪里放大
+                //可以假设Y的偏移为0 辅助理解
+                offsetX = (getWidth() / 2f - e.getX()) * bigScale / smallScale - (getWidth() / 2f - e.getX());
+                offsetY = (getHeight() / 2f - e.getY()) * bigScale / smallScale - (getHeight() / 2f - e.getY());
+                animation.start();
+            }else{
+                animation.reverse();
+            }
+            return false;
+        }
+
+        //双击 同时还检测双击后的事件
+        @Override
+        public boolean onDoubleTapEvent(MotionEvent e) {
+            return false;
+        }
+
+    }
+    //惯性滑动的辅助类
+    private class MyRunnable implements Runnable{
+        @Override
+        public void run() {
+            if(scroller.computeScrollOffset()){
+                offsetX = scroller.getCurrX();
+                offsetY = scroller.getCurrY();
+                invalidate();
+                postOnAnimation(this);
+            }
+        }
     }
 
-    @Override
-    public void onShowPress(MotionEvent e) {
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent e) {
-        return false;
-    }
-
-    //滑动
-    @Override
-    public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-        if(isBig){
-            offsetX = offsetX - distanceX;
-            offsetY = offsetY - distanceY;
+    //双指缩放
+    private class ScaleListener implements ScaleGestureDetector.OnScaleGestureListener{
+        private float preScaleRate;
+        //缩放过程
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            float f = detector.getScaleFactor();
+            scaleMultiple = preScaleRate * f;
             invalidate();
+            return false;
         }
-        return false;
-    }
 
-    @Override
-    public void onLongPress(MotionEvent e) {
-
-    }
-
-    //惯性滑动
-    @Override
-    public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
-        scroller.fling((int)offsetX,(int)offsetY,(int)velocityX,(int)velocityY,
-                -(int)(bitmap.getWidth()*bigScale - getWidth()),
-                (int)(bitmap.getWidth()*bigScale - getWidth()),
-                -(int)(bitmap.getHeight()*bigScale - getHeight()),
-                (int)(bitmap.getHeight()*bigScale - getHeight()));
-        postOnAnimation(this);
-        return false;
-    }
-
-    //确认单击[第一次按下后，一段时间内(1500ms貌似)没有第二次点击]
-    @Override
-    public boolean onSingleTapConfirmed(MotionEvent e) {
-        return false;
-    }
-
-    //双击
-    @Override
-    public boolean onDoubleTap(MotionEvent e) {
-        isBig = !isBig;
-        if(isBig){
-            //点击放大时，点到哪里就从哪里放大
-            //可以假设Y的偏移为0 辅助理解
-            offsetX = (getWidth() / 2f - e.getX()) * bigScale / smallScale - (getWidth() / 2f - e.getX());
-            offsetY = (getHeight() / 2f - e.getY()) * bigScale / smallScale - (getHeight() / 2f - e.getY());
-            animation.start();
-        }else{
-            animation.reverse();
+        //缩放开始
+        @Override
+        public boolean onScaleBegin(ScaleGestureDetector detector) {
+            //要return 才能调用 onScale
+            preScaleRate = scaleMultiple;
+            return true;
         }
-        return false;
-    }
+        //缩放结束
+        @Override
+        public void onScaleEnd(ScaleGestureDetector detector) {
 
-    //双击 同时还检测双击后的事件
-    @Override
-    public boolean onDoubleTapEvent(MotionEvent e) {
-        return false;
+        }
     }
-
+    /**
+     * 给x y 的偏移做限制，防止图片划出屏幕
+     */
     private void reviseXY(){
         final float over = Util.dpToPixel(0);
         //往右滑[负数取最大值 正数取最小值 往中间走]
@@ -168,13 +227,5 @@ public class ScalableImageView extends View implements GestureDetector.OnGesture
         offsetY = Math.max(offsetY ,-(bitmap.getHeight()*bigScale - getHeight()) / 2 - over);
     }
 
-    @Override
-    public void run() {
-        if(scroller.computeScrollOffset()){
-            offsetX = scroller.getCurrX();
-            offsetY = scroller.getCurrY();
-            invalidate();
-            postOnAnimation(this);
-        }
-    }
+
 }
